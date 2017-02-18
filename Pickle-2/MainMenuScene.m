0c2dffd74c990021f2c710499daaa7b121b2e474
b42thomas <b42thomas@gmail.com>
2017-02-18 15:39:52 -0500

    Working on Main Menu

--- Pickle-2/MainMenuScene.m
+++ Pickle-2/MainMenuScene.m
@@ -6,6 +6,8 @@
 //  Copyright © 2017 BIZNIT. All rights reserved.
 //
 
+//code to create main menu goes here
+
 #import "MainMenuScene.h"
 
 
@@ -29,24 +31,73 @@
 {
     self.backgroundColor = [SKColor blueColor];
     self.scaleMode = SKSceneScaleModeAspectFit;
-    [self addChild: [self newMainMenu]];
+    [self addChild: [self newMainMenu]];  //adds main menu to scene
 }
 
--(SKSpriteNode *)newMainMenu
+-(SKSpriteNode *)newMainMenu  //method to create mainMenu Background image squraes should be filled in image
 {
    SKSpriteNode *mainMenu = [SKSpriteNode spriteNodeWithImageNamed:@"MainMenu"];
-    mainMenu.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
-    //mainMenu.scale
+    mainMenu.position = CGPointMake(CGRectGetMinX(self.frame),CGRectGetMinY(self.frame));
+    mainMenu.size = self.size;
+    mainMenu.anchorPoint = CGPointMake(0,0);
+    SKSpriteNode *playButton = [self addPlayButton];  //create playbutton sprite
+    SKLabelNode *highScore = [self addHighScore];  //create highscore sprite
+    SKLabelNode *lastScore = [self addLastScore];  //create lastscore sprite
+ 
+    [mainMenu addChild:playButton];
+    [mainMenu addChild:highScore];
+    [mainMenu addChild:lastScore];
+    
+    //place child nodes
+    CGRect parentFrame = playButton.parent.frame;
+    CGFloat x = CGRectGetMaxX(parentFrame);
+    CGFloat y = CGRectGetMaxY(parentFrame);
+    CGPoint pbpos = CGPointMake( x*0.455, y*0.315);
+    playButton.position = pbpos;
+    
+    CGPoint hspos = CGPointMake( x*0.25, y*0.5);
+    highScore.position = hspos;
+    
+    CGPoint lspos = CGPointMake( x*0.75, y*0.5);
+    lastScore.position = lspos;
+    
+    
+    
+    
     return mainMenu;
 }
 
-- (SKLabelNode *)newHelloNode
+- (SKSpriteNode *)addPlayButton  //play button, needs a new image with white background
 {
-    SKLabelNode *helloNode = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    helloNode.text = @"Hello, World!";
-    helloNode.fontSize = 42;
-    helloNode.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
-    return helloNode;
+    SKSpriteNode *playButton = [SKSpriteNode spriteNodeWithImageNamed:@"PlayButton"];
+
+    playButton.anchorPoint = CGPointMake(0,0);
+    playButton.xScale = 1;
+    playButton.yScale = 1;
+    return playButton;
 }
+
+
+- (SKLabelNode *)addHighScore
+{
+    SKLabelNode *highScore = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    highScore.color = [SKColor whiteColor];
+    highScore.text = @"99";
+    highScore.fontSize = 80;
+       return highScore;
+}
+
+
+- (SKLabelNode *)addLastScore
+{
+    SKLabelNode *lastScore = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    lastScore.color = [SKColor whiteColor];
+    lastScore.text = @"00";
+    lastScore.fontSize = 80;
+    lastScore.position = CGPointMake(CGRectGetMidX(lastScore.parent.frame) + 160,CGRectGetMidY(lastScore.parent.frame)-15);
+    return lastScore;
+}
+
+
 
 @end
