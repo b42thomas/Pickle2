a7579d1e35bc74670fbf84bb476cc25d5a7024ee
b42thomas <b42thomas@gmail.com>
2017-02-19 13:18:10 -0500

    1:17pm still finishing main menu layout

--- Pickle-2/MainMenuScene.m
+++ Pickle-2/MainMenuScene.m
@@ -47,6 +47,8 @@
     SKSpriteNode *gear = [self addGear];//create settings sprite
     SKSpriteNode *soundSettings = [self addsoundSettings];//create settings sprite
     SKSpriteNode *characterSelectButton = [self addCharacterSelectButton];//create Character Select Sprite
+    SKSpriteNode *characterMenu = [self addCharacterMenu];//create Character Menu
+    
  
     [mainMenu addChild:playButton];
     [mainMenu addChild:highScore];
@@ -54,6 +56,7 @@
     [mainMenu addChild:gear];
     //[mainMenu addChild:soundSettings];
     [mainMenu addChild:characterSelectButton];
+    [mainMenu addChild:characterMenu];
     
     
     
@@ -73,20 +76,23 @@
     //position for sound settings button
     CGPoint gpos = CGPointMake( x*0.23, y*0.22);
     gear.position = gpos;
-    //position for soundSettings
+    //position for soundSettings and Character menu
     CGPoint sspos = CGPointMake( x*0.25, y*0.25);
     soundSettings.position = sspos;
+    characterMenu.position = sspos;
     //position for character Select button
     CGPoint csbpos = CGPointMake( x*0.73, y*0.22);
     characterSelectButton.position = csbpos;
+    
     
     
     //this line is KEY for creating displays that look the same on all iPhones!!!!!!!!
     playButton.size = CGSizeMake(parentFrame.size.width*0.10,parentFrame.size.height*0.175);
     //gear size
     gear.size = CGSizeMake(parentFrame.size.width*0.05,parentFrame.size.height*0.09);
-    //sound settings menu size
+    //sound settings menu size and character menu size
     soundSettings.size = CGSizeMake(parentFrame.size.width*0.45,parentFrame.size.height*0.45);
+    characterMenu.size = CGSizeMake(parentFrame.size.width*0.45,parentFrame.size.height*0.45);
     //character select button size
     characterSelectButton.size = CGSizeMake(parentFrame.size.width*0.05,parentFrame.size.height*0.09);
     
@@ -141,21 +147,35 @@
     [soundSettings setName:@"soundSettings"];
     soundSettings.anchorPoint = CGPointMake(0,0);
     soundSettings.size = CGSizeMake(self.frame.size.width*0.45,self.frame.size.height*0.45);
+    
     SKSpriteNode *musicToggle = [self addBox];
     SKSpriteNode *soundToggle = [self addBox];
+    SKSpriteNode *exitButton = [SKSpriteNode spriteNodeWithImageNamed:@"ExitButton"];
+    
     [musicToggle setName:@"musicToggle"];
     [soundToggle setName:@"soundToggle"];
+    [exitButton setName:@"exitButtonSSM"];
+    
     [soundSettings addChild:musicToggle];
     [soundSettings addChild:soundToggle];
+    [soundSettings addChild:exitButton];
 
     CGRect parentFrame = soundSettings.frame;
     CGFloat x = CGRectGetMaxX(parentFrame);
     CGFloat y = CGRectGetMaxY(parentFrame);
+   
+    //sound toggle box pos and size
     soundToggle.position = CGPointMake(x*0.75, y*0.35);
     soundToggle.size = CGSizeMake(parentFrame.size.width*0.15,parentFrame.size.height*0.23);
-    
+    //msuci toggle box pos and sie
     musicToggle.position = CGPointMake(x*0.75, y*0.7);
     musicToggle.size = CGSizeMake(parentFrame.size.width*0.15,parentFrame.size.height*0.23);
+    //exit button pos and size
+    exitButton.anchorPoint = CGPointMake(1, 1);
+    exitButton.position = CGPointMake(x*0, y);
+    exitButton.size = CGSizeMake(parentFrame.size.width*0.15,parentFrame.size.height*0.23);
+    
+    
 
     
     return soundSettings;
@@ -177,6 +197,48 @@
     characterSelectButton.anchorPoint = CGPointMake(0,0);
     return characterSelectButton;
 }
+
+-(SKSpriteNode*)addCharacterMenu
+{
+    SKSpriteNode *characterMenu = [SKSpriteNode spriteNodeWithImageNamed:@"characterMenu"];
+    [characterMenu setName:@"characterMenu"];
+    characterMenu.anchorPoint = CGPointMake(0,0);
+    characterMenu.size = CGSizeMake(self.frame.size.width*0.45,self.frame.size.height*0.45);
+    
+    SKSpriteNode *exitButton = [SKSpriteNode spriteNodeWithImageNamed:@"ExitButton"];
+    SKSpriteNode *leftButton = [SKSpriteNode spriteNodeWithImageNamed:@"PlayButton"];
+    SKSpriteNode *rightButton = [SKSpriteNode spriteNodeWithImageNamed:@"PlayButton"];
+    
+    [exitButton setName:@"exitButtonSSM"];
+    [leftButton setName:@"leftButton"];
+    [rightButton setName:@"rightButton"];
+    
+    [characterMenu addChild:exitButton];
+    [characterMenu addChild:leftButton];
+    //[characterMenu addChild:rightButton];
+    
+    CGRect parentFrame = characterMenu.frame;
+    CGFloat x = CGRectGetMaxX(parentFrame);
+    CGFloat y = CGRectGetMaxY(parentFrame);
+    
+    //exit button pos and size
+    exitButton.anchorPoint = CGPointMake(1, 1);
+    exitButton.position = CGPointMake(x*0, y);
+    exitButton.size = CGSizeMake(parentFrame.size.width*0.15,parentFrame.size.height*0.23);
+    //left button pos and size
+    leftButton.anchorPoint = CGPointMake(0.5, 0.5);
+    leftButton.position = CGPointMake(x*0.15, y*0.5);
+    leftButton.size = CGSizeMake(-(parentFrame.size.width*0.15),parentFrame.size.height*0.23);
+    
+    
+    
+    
+    return characterMenu;
+    
+    
+    
+}
+
 
 
 @end
